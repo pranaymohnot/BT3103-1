a9a29c586729c6f444b839deb94f3131d9786f6c
pranaymohnot <50563784+pranaymohnot@users.noreply.github.com>
2020-03-17 18:35:20 +0800

    Update firebase.js

--- grocerychart/src/firebase.js
+++ grocerychart/src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-    //Key in your database details
-    apiKey: "AIzaSyDR5ZGNKSbpZ71uXOtH99-tX5qSWzgBr9M",
-    authDomain: "bt3103-2020.firebaseapp.com",
-    databaseURL: "https://bt3103-2020.firebaseio.com",
-    projectId: "bt3103-2020"
-
-  };
+const firebaseConfig = {};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
